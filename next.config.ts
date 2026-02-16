a35f3939a1c48b6258a36e4058b5ad6bc40b850b
oyiokux <oyiokux@oyixiazai.com>
2026-02-17 02:34:25 +0800

    Configure GitLab Pages deployment: enabling static export

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // 启用静态导出
+  images: {
+    unoptimized: true, // 静态站点无法使用 Next.js 默认图片优化
+  },
 };
 
 export default nextConfig;
